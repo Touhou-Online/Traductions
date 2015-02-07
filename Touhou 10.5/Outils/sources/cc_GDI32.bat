149977509b8f6a00d0621a454e61795f845eb91b
Bruno Liron <bruno.liron@epitech.eu>
2015-02-07 21:09:16 +0100

    Mise en ligne de la bonne version de cc_GDI32.bat (celle avec documentation)

--- Touhou 10.5/Outils/sources/cc_GDI32.bat
+++ Touhou 10.5/Outils/sources/cc_GDI32.bat
@@ -1,6 +1,10 @@
-@prompt -----^> 
+﻿@rem Pour compiler ceci, vous devez installer MinGW et NASM, les mettre
+@rem dans votre PATH, puis lancer ce fichier .bat
+@rem Aussi, dans la ligne exécutant ld plus bas, remplacez
+@rem E:\Program_Files\MinGW\lib par le chemin d'accès vers les libs de MinGW
+@rem sur votre ordinateur.
+
 gcc -c code.c -Wall -Wextra
 nasm gdi32.asm -f Win32
 ld -o i32.dll -shared gdi32.obj gdi32.def code.o -LE:\Program_Files\MinGW\lib -lkernel32 -lgdi32 -lmsvcrt -e _DllMain@12
 @del gdi32.obj code.o 2>nul
-@prompt ^> 
